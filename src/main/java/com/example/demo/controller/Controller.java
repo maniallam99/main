fffbf0d5d8921ec65bf1774b726e484b0e50a2da
mani <maniallam99@gmail.com>
2023-06-08 14:46:21 +0530

    commit-1

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -33,16 +33,17 @@ public class Controller {
     String l="change in 2";
 
 
-
     String o="change in 1";
     String n="change in 2";
     String p="change in 2";
-    String r="change in 2";
     String q="change in 1";
     String s="change in 1";
     String u="change in 1";
     String u="change in 1";
     String t="change in 2";
+
+    String t="change in 2";
+
 
 
 
